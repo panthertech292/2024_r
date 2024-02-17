ba26f1c21e25986e12eb006c1519013142b470ca
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-02-17 16:00:38 -0500

    Added YAGSL

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,8 +17,15 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static final double ROBOT_MASS = 46.2664; //kg, not lbs
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME  = 0.12; //s, 20ms + 110ms sprk max velocity lag
+
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final double kDeadband = 0.05;
   }
 
   public static class IntakeConstants {
