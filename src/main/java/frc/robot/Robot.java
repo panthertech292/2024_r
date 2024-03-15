87cea21fa187f6146c22a02c19c0f51a922ace22
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-03-15 00:59:32 -0400

    Add port forwarding

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,6 +25,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Port forwading for limelight usage via USB
+    for (int port = 5800; port <= 5807; port++) {
+      PortForwarder.add(port, "limelight.local", port);
+    }
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
