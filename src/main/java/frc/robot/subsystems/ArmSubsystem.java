311dedc401c9d6cb754d5be63bd08ad6b0337c6c
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-03-17 15:16:06 -0400

    mid playoff

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -127,6 +127,7 @@ public class ArmSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
     //SmartDashboard.putNumber("RAW Arm Position: ", RotationAngleEncoder.getAbsolutePosition());
     SmartDashboard.putNumber("Arm Position", getRotationAngle());
+    SmartDashboard.putBoolean("Arm ready to Intake:", isArmReadyToIntake());
     SmartDashboard.putBoolean("Arm sensor active", getRotationDownSwitch());
   }
 }
