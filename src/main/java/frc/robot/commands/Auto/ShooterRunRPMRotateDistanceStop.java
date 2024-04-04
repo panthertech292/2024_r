5772a46bc92d4b5e0a6f2b1be6da6e4461b1d0df
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-04-04 00:37:38 -0400

    Clean up shooter commands

--- src/main/java/frc/robot/commands/Auto/ShooterRunRPMRotateDistanceStop.java
+++ src/main/java/frc/robot/commands/Auto/ShooterRunRPMRotateDistanceStop.java
@@ -5,7 +5,6 @@
 package frc.robot.commands.Auto;
 
 import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.InterpolationConstants;
 import frc.robot.subsystems.ArmSubsystem;
@@ -13,30 +12,30 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class ShooterRunRPMRotateDistanceStop extends Command {
-  //Shooting
+  // Shooting
   private final ShooterSubsystem ShooterSub;
   private double shooterSpeed;
   private double beltSpeed;
   private double shooterRPM;
-  private double [] last10Values;
+  private double[] last10Values;
   private int index;
   private boolean readyToFire;
-  private boolean setInitalMovingAt; //Used for debug, can be removed if needed
+  private boolean setInitalMovingAt;
 
-  //Rotation
+  // Rotation
   private final ArmSubsystem ArmSub;
   private double error;
   private double p;
   private double minSpeed;
 
-  //Distance
+  // Distance
   private SwerveSubsystem SwerveSub;
   private double distance;
   private double angle;
 
   private boolean finished;
-  //private Timer time;
   private double noteOutTime;
+
   /** Creates a new RunShooterRPM. */
   public ShooterRunRPMRotateDistanceStop(ShooterSubsystem s_ShooterSubsystem, ArmSubsystem s_ArmSubsystem, SwerveSubsystem s_SwerveSubsystem, double shooterSpeed, double beltSpeed, double p, double minSpeed) {
     ShooterSub = s_ShooterSubsystem;
@@ -48,14 +47,11 @@ public class ShooterRunRPMRotateDistanceStop extends Command {
     index = 0;
     distance = SwerveSub.getDistanceFromSpeaker();
     angle = InterpolationConstants.angleMap.get(distance);
-    
-    //Rotation
+
+    // Rotation
     this.p = p;
     this.minSpeed = minSpeed;
 
-    
-    //time.reset();
-    
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(s_ShooterSubsystem);
   }
@@ -66,7 +62,6 @@ public class ShooterRunRPMRotateDistanceStop extends Command {
     ShooterSub.setShooter(shooterSpeed);
     setInitalMovingAt = false;
     finished = false;
-    //noteOutTime = 999999999;
     last10Values[0] = 0;
     last10Values[9] = 1000;
     System.out.println("ShooterRunRPMRotateDistanceStop: Start Auto Shoot");
@@ -75,66 +70,68 @@ public class ShooterRunRPMRotateDistanceStop extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //SmartDashboard.putNumber("SHOOTER RPM", angle);
     distance = SwerveSub.getDistanceFromSpeaker();
     angle = InterpolationConstants.angleMap.get(distance);
 
-    error = (angle - ArmSub.getRotationAngle())*p;
-    if (minSpeed > Math.abs(error)){//If we are running too slow, go at a min speed
-      if (error > 0){//Checks sign of error, sets minspeed to either + or -
+    error = (angle - ArmSub.getRotationAngle()) * p;
+    if (minSpeed > Math.abs(error)) {// If we are running too slow, go at a min speed
+      if (error > 0) {// Checks sign of error, sets minspeed to either + or -
         error = minSpeed;
-      }else{
+      } else {
         error = -minSpeed;
       }
     }
     ArmSub.setArmRotate(error);
     readyToFire = true;
-    //Only run below if arm is in position
-    if(Math.abs(error) > 0.03){
+    // Check to see if arm is in position
+    if (Math.abs(error) > 0.03) {
       readyToFire = false;
     }
-      /* Basically this creates an array of the last 10 recorded shooter RPMS
-        It will only shoot the note once RPMs have stabilized.
-        Stabilized means the 10 recorded values are within 25 RPMs of the current shooter RPM
-        This really could be acomplished with a PID, but I'm lazy.
-      */
+    /*
+     * Basically this creates an array of the last 10 recorded shooter RPMS
+     * It will only shoot the note once RPMs have stabilized.
+     * Stabilized means the 10 recorded values are within 25 RPMs of the current
+     * shooter RPM
+     * This really could be acomplished with a PID, but I'm lazy.
+     */
 
-      shooterRPM = ShooterSub.getShooterLowEncoderVelocity();
-      last10Values[index] = shooterRPM;
-      
-      for (int i = 0; i < 10; i++){
-        if(Math.abs(shooterRPM - last10Values[i]) > 25){
-          readyToFire = false;
-        }
-      }
-      //Make sure we don't fire early
-      if(shooterRPM < 1000 & shooterSpeed > 0.49){
+    shooterRPM = ShooterSub.getShooterLowEncoderVelocity();
+    last10Values[index] = shooterRPM;
+
+    for (int i = 0; i < 10; i++) {
+      if (Math.abs(shooterRPM - last10Values[i]) > 25) {
         readyToFire = false;
       }
+    }
+    // Make sure we don't fire early
+    if (shooterRPM < 1000 & shooterSpeed > 0.49) {
+      readyToFire = false;
+    }
 
-      if(readyToFire){
-        ShooterSub.setFeedBelts(beltSpeed);
-        if(!setInitalMovingAt){//debug
-          setInitalMovingAt = true;
-          noteOutTime = Timer.getFPGATimestamp();
-          System.out.println("ShooterRunRPMRotateDistanceStop: Started advancing belts forward @: " + shooterRPM + " With: Interpolated Angle: " + angle + " at time: " + noteOutTime + " at a distance of " + distance);
-        }
-        if(ShooterSub.getShooterSwitch()){
-          System.out.println("ShooterRunRPMRotateDistanceStop: Finished due to detecing out note");
+    if (readyToFire) {
+      ShooterSub.setFeedBelts(beltSpeed);
+      if (!setInitalMovingAt) {
+        setInitalMovingAt = true;
+        noteOutTime = Timer.getFPGATimestamp();
+        System.out.println("ShooterRunRPMRotateDistanceStop: Started advancing belts forward @: " + shooterRPM
+            + " With: Interpolated Angle: " + angle + " at time: " + noteOutTime + " at a distance of " + distance);
+      }
+      if (ShooterSub.getShooterSwitch()) {
+        System.out.println("ShooterRunRPMRotateDistanceStop: Finished due to detecing out note");
+        finished = true;
+      }
+
+      if (!ShooterSub.getFeedBeltSwitch()) {
+        if (Timer.getFPGATimestamp() > noteOutTime + 0.15) {
+          System.out.println("ShooterRunRPMRotateDistanceStop: Finished at time: " + Timer.getFPGATimestamp());
           finished = true;
         }
-        
-        if(!ShooterSub.getFeedBeltSwitch()){
-          if(Timer.getFPGATimestamp() > noteOutTime + 0.15){
-            System.out.println("ShooterRunRPMRotateDistanceStop: Finished at time: " + Timer.getFPGATimestamp());
-            finished = true;
-          }
-          
-        }
-        
+
       }
-      index++;
-      index = index % 10;
+
+    }
+    index++;
+    index = index % 10;
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/Shooter/ShooterRunRPMRotateDistance.java
+++ src/main/java/frc/robot/commands/Shooter/ShooterRunRPMRotateDistance.java
@@ -11,26 +11,27 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class ShooterRunRPMRotateDistance extends Command {
-  //Shooting
+  // Shooting
   private final ShooterSubsystem ShooterSub;
   private double shooterSpeed;
   private double beltSpeed;
   private double shooterRPM;
-  private double [] last10Values;
+  private double[] last10Values;
   private int index;
   private boolean readyToFire;
-  private boolean setInitalMovingAt; //Used for debug, can be removed if needed
+  private boolean setInitalMovingAt;
 
-  //Rotation
+  // Rotation
   private final ArmSubsystem ArmSub;
   private double error;
   private double p;
   private double minSpeed;
 
-  //Distance
+  // Distance
   private SwerveSubsystem SwerveSub;
   private double distance;
   private double angle;
+
   /** Creates a new RunShooterRPM. */
   public ShooterRunRPMRotateDistance(ShooterSubsystem s_ShooterSubsystem, ArmSubsystem s_ArmSubsystem, SwerveSubsystem s_SwerveSubsystem, double shooterSpeed, double beltSpeed, double p, double minSpeed) {
     ShooterSub = s_ShooterSubsystem;
@@ -42,11 +43,11 @@ public class ShooterRunRPMRotateDistance extends Command {
     index = 0;
     distance = SwerveSub.getDistanceFromSpeaker();
     angle = InterpolationConstants.angleMap.get(distance);
-    
-    //Rotation
+
+    // Rotation
     this.p = p;
     this.minSpeed = minSpeed;
-    
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(s_ShooterSubsystem);
   }
@@ -67,53 +68,53 @@ public class ShooterRunRPMRotateDistance extends Command {
     distance = SwerveSub.getDistanceFromSpeaker();
     angle = InterpolationConstants.angleMap.get(distance);
 
-    error = (angle - ArmSub.getRotationAngle())*p;
-    if (minSpeed > Math.abs(error)){//If we are running too slow, go at a min speed
-      if (error > 0){//Checks sign of error, sets minspeed to either + or -
+    error = (angle - ArmSub.getRotationAngle()) * p;
+    if (minSpeed > Math.abs(error)) {// If we are running too slow, go at a min speed
+      if (error > 0) {// Checks sign of error, sets minspeed to either + or -
         error = minSpeed;
-      }else{
+      } else {
         error = -minSpeed;
       }
     }
     ArmSub.setArmRotate(error);
 
-    //Only run below if arm is in position
-    //if(Math.abs(error) < 0.02){
-      /* Basically this creates an array of the last 10 recorded shooter RPMS
-        It will only shoot the note once RPMs have stabilized.
-        Stabilized means the 10 recorded values are within 25 RPMs of the current shooter RPM
-        This really could be acomplished with a PID, but I'm lazy.
-      */
-      readyToFire = true;
-    //Only run below if arm is in position
-    if(Math.abs(error) > 0.03){
+    readyToFire = true;
+    // Only run below if arm is in position
+    if (Math.abs(error) > 0.03) {
       readyToFire = false;
     }
 
-      shooterRPM = ShooterSub.getShooterLowEncoderVelocity();
-      last10Values[index] = shooterRPM;
-      //readyToFire = true; //TODO: We might want to set this to false depending on arm angle (In case we are shooting too early).
-      for (int i = 0; i < 10; i++){
-        if(Math.abs(shooterRPM - last10Values[i]) > 25){
-          readyToFire = false;
-        }
-      }
-      //Make sure we don't fire early
-      if(shooterRPM < 1000 & shooterSpeed > 0.49){
+    /*
+     * Basically this creates an array of the last 10 recorded shooter RPMS
+     * It will only shoot the note once RPMs have stabilized.
+     * Stabilized means the 10 recorded values are within 25 RPMs of the current
+     * shooter RPM
+     * This really could be acomplished with a PID, but I'm lazy.
+     */
+    
+    shooterRPM = ShooterSub.getShooterLowEncoderVelocity();
+    last10Values[index] = shooterRPM;
+    for (int i = 0; i < 10; i++) {
+      if (Math.abs(shooterRPM - last10Values[i]) > 25) {
         readyToFire = false;
       }
+    }
+    // Make sure we don't fire early
+    if (shooterRPM < 1000 & shooterSpeed > 0.49) {
+      readyToFire = false;
+    }
 
-      if(readyToFire){
-        ShooterSub.setFeedBelts(beltSpeed);
-        if(!setInitalMovingAt){//debug
-          setInitalMovingAt = true;
-          System.out.println("ShooterRunRPMRotateDistance: Started advancing belts forward @: " + shooterRPM + " With: Interpolated Angle: " + angle);
-        }
-        
+    if (readyToFire) {
+      ShooterSub.setFeedBelts(beltSpeed);
+      if (!setInitalMovingAt) {// debug
+        setInitalMovingAt = true;
+        System.out.println("ShooterRunRPMRotateDistance: Started advancing belts forward @: " + shooterRPM + " With: Interpolated Angle: " + angle);
       }
-      index++;
-      index = index % 10;
-    //}
+
+    }
+    index++;
+    index = index % 10;
+    // }
   }
 
   // Called once the command ends or is interrupted.
